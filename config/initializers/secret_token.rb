ec24303befc55ba63cf7cbf5642254a849a7f63c
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-12-26 15:48:19 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fronnect::Application.config.secret_key_base = '38c8527d16f6f50e2ec387569f55f65c40b58b6eca1ee4d902f3d0bdb373cd1ee88ee4e2c2278b59c0f6ec57c167e2f1eafe4595e91cd3a7b48a73a490986926'
