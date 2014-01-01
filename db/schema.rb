246b21fd058fc0e47e5d4b77443cc4536864dccd
Simon Taranto <simon.taranto@gmail.com>
2014-01-01 08:35:33 -0700

    create attendees and I'm in button

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131229175007) do
+ActiveRecord::Schema.define(version: 20140101150925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendees", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "events", force: true do |t|
     t.integer  "user_id"
