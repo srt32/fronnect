f4b54617dbd3ddf4a7fa35f7163fbf8ffd32c499
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-01-07 22:02:57 -0700

    Hide I'm in button after a user has RSVPd

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@ class User < ActiveRecord::Base
     #Event.where(:id => attendees.map(&:event_id))
     Event.where(:id => Attendee.where(:user_id => id).map(&:event_id))
   end
+
+  def attendee?(event)
+    Attendee.exists?(event_id: event.id, user_id: self.id)
+  end
 end
