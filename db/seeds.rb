1a0eb57f9f9996a6002cd0d46ea618d33cddeddb
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-01-17 16:28:45 -0700

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,9 @@ user1 = User.create(email: "simon@example.com", first_name: "Simon", last_name: 
 user2 = User.create(email: "nikhil@example.com", first_name: "Nikhil", last_name: "Taneja", username: "ntaneja", password: "password")
 
 
-event1 = user1.events.create(title: "Coding", description: "Coding at Galvanize", venue: "Galvanize", address: "1062 Delaware St, Denver, CO", starts_at: "2014-01-19 12:00:00")
-event2 = user2.events.create(title: "Skiing", description: "Take advantage of all the fresh powder at Breck", venue: "Breckenridge Ski Resort", address: "Breckenridge, CO", starts_at: "2014-01-18 06:00:00")
-event3 = user2.events.create(title: "Lunch", description: "Group lunch!", venue: "Zocalo", address: "12 E 11th Ave, Denver, CO 80203", starts_at: "2014-01-17 12:00:00")
+event1 = user1.events.create(title: "Coding", description: "Coding at Galvanize", venue: "Galvanize", address: "1062 Delaware St, Denver, CO", starts_at: "2014-01-26 12:00:00")
+event2 = user2.events.create(title: "Skiing", description: "Take advantage of all the fresh powder at Breck", venue: "Breckenridge Ski Resort", address: "Breckenridge, CO", starts_at: "2014-01-25 06:00:00")
+event3 = user2.events.create(title: "Lunch", description: "Group lunch!", venue: "Zocalo", address: "12 E 11th Ave, Denver, CO 80203", starts_at: "2014-01-24 12:00:00")
+event3 = user2.events.create(title: "Coffee", description: "Coffee shop I've been wanting to check out", venue: "Roostercat", address: "", starts_at: "2014-01-23 12:00:00")
 
 
